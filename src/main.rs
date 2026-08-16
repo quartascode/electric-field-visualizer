4d5570dfca86bb8c7be4fa264a92029ec18fae72
quartascode <brenoposavac@proton.me>
2026-08-16 20:21:00 -0300

    add arrow head

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,19 @@
 use macroquad::{prelude::*};
 use electric_field::{reverse_projection, project_to_screen, grid, particle, SCALE, WIDTH, HEIGHT};
 
+const GRID_AMOUNT_HORIZONTAL: u32 = 48;
+
 #[macroquad::main(window_conf())]
 async fn main() {
-    let p1 = particle::Particle::new(Vec2 { x: -10.0, y:  10.0 }, -1.0);
-    let p2 = particle::Particle::new(Vec2 { x:  10.0, y: -10.0 },  1.0);
+    let p1 = particle::Particle::new(Vec2 { x: -10.0, y:  10.0 }, -0.5);
+    let p2 = particle::Particle::new(Vec2 { x:  10.0, y: -10.0 },  0.5);
 
     let mut particles = Vec::new();
 
     particles.push(p1);
     particles.push(p2);
 
-    let mut grid = grid::Grid::new(64);
+    let mut grid = grid::Grid::new(GRID_AMOUNT_HORIZONTAL);
 
     show_mouse(false);
     loop {
@@ -47,7 +49,7 @@ async fn main() {
         let max = 5000.0;
         let min = 0.0;
         for cell in &grid.cells {
-            let pos = project_to_screen(cell.pos, SCALE);
+            let screen_pos = project_to_screen(cell.pos, SCALE);
 
             // tone it down a bit
             //let field = cell.field / (KE / 100.0);
@@ -60,9 +62,23 @@ async fn main() {
             // make sure the lines arent REALLY big
             let field = field.clamp_length(0.0, 2.0);
 
-            let vec_end = project_to_screen(cell.pos + field, SCALE);
+            let vec_end = cell.pos + field;
+            let screen_vec_end = project_to_screen(vec_end, SCALE);
 
-            draw_line(pos.x, pos.y, vec_end.x, vec_end.y, 1.0, color);
+            let dir = (vec_end - cell.pos).normalize();
+            let perp = Vec2 { x: -dir.y, y: dir.x };
+            let l = 0.75;
+            let c = vec_end - dir * l;
+            let arrow1 = c + perp * l * 0.5;
+            let arrow2 = c - perp * l * 0.5;
+            let arrow1 = project_to_screen(arrow1, SCALE);
+            let arrow2 = project_to_screen(arrow2, SCALE);
+
+            let girth = 1.5;
+            draw_line(screen_vec_end.x, screen_vec_end.y, arrow1.x, arrow1.y, girth, color);
+            draw_line(screen_vec_end.x, screen_vec_end.y, arrow2.x, arrow2.y, girth, color);
+
+            draw_line(screen_pos.x, screen_pos.y, screen_vec_end.x, screen_vec_end.y, girth, color);
         }
 
         for p in &particles {
