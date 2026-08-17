0c214e0bce3f81857fb0942ec0ef3334e62e6502
quartascode <brenoposavac@proton.me>
2026-08-17 13:51:11 -0300

    make grid draw method

--- src/grid.rs
+++ src/grid.rs
@@ -1,10 +1,9 @@
 use macroquad::{prelude::*};
+use crate::{project_to_screen, reverse_projection};
 
 use crate::WIDTH;
 use crate::HEIGHT;
 use crate::SCALE;
-
-use crate::reverse_projection;
 
 pub struct Grid {
     pub length: u32,
@@ -40,6 +39,46 @@ impl Grid {
             length: grid_length,
             height: grid_height,
             cells: cells,
+        }
+    }
+
+    pub fn draw(&self) {
+        let max = 5000.0;
+        let min = 0.0;
+        for cell in &self.cells {
+            let screen_pos = project_to_screen(cell.pos, SCALE);
+
+            // tone it down a bit
+            //let field = cell.field / (KE / 100.0);
+            let field = cell.field;
+            let module = field.length().sqrt();
+            if module < 1.0 {
+                continue;
+            }
+
+            let t = (module - min) / (max - min);
+            let color = Color::new(t, 0.0, 1.0-t, 1.0);
+
+            // make sure the lines arent REALLY big
+            let field = field.clamp_length(0.0, 2.0);
+
+            let vec_end = cell.pos + field;
+            let screen_vec_end = project_to_screen(vec_end, SCALE);
+
+            let dir = (vec_end - cell.pos).normalize();
+            let perp = Vec2 { x: -dir.y, y: dir.x };
+            let l = 0.75;
+            let c = vec_end - dir * l;
+            let arrow1 = c + perp * l * 0.5;
+            let arrow2 = c - perp * l * 0.5;
+            let arrow1 = project_to_screen(arrow1, SCALE);
+            let arrow2 = project_to_screen(arrow2, SCALE);
+
+            let girth = 1.5;
+            draw_line(screen_vec_end.x, screen_vec_end.y, arrow1.x, arrow1.y, girth, color);
+            draw_line(screen_vec_end.x, screen_vec_end.y, arrow2.x, arrow2.y, girth, color);
+
+            draw_line(screen_pos.x, screen_pos.y, screen_vec_end.x, screen_vec_end.y, girth, color);
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,14 @@ const GRID_AMOUNT_HORIZONTAL: u32 = 48;
 #[macroquad::main(window_conf())]
 async fn main() {
     let p1 = particle::Particle::new(Vec2 { x: -10.0, y:  10.0 }, -0.5);
-    let p2 = particle::Particle::new(Vec2 { x:  10.0, y: -10.0 },  0.5);
+    let p2 = particle::Particle::new(Vec2 { x:  50.0, y: -25.0 },  0.5);
+    let p3 = particle::Particle::new(Vec2 { x:   0.0, y:  25.0 },  0.5);
 
     let mut particles = Vec::new();
 
     particles.push(p1);
     particles.push(p2);
+    particles.push(p3);
 
     let mut grid = grid::Grid::new(GRID_AMOUNT_HORIZONTAL);
 
@@ -46,44 +48,11 @@ async fn main() {
         // draw
         clear_background(BLACK);
 
-        let max = 5000.0;
-        let min = 0.0;
-        for cell in &grid.cells {
-            let screen_pos = project_to_screen(cell.pos, SCALE);
-
-            // tone it down a bit
-            //let field = cell.field / (KE / 100.0);
-            let field = cell.field;
-            let module = field.length().sqrt();
-
-            let t = (module - min) / (max - min);
-            let color = Color::new(t, 0.0, 1.0-t, 1.0);
-
-            // make sure the lines arent REALLY big
-            let field = field.clamp_length(0.0, 2.0);
-
-            let vec_end = cell.pos + field;
-            let screen_vec_end = project_to_screen(vec_end, SCALE);
-
-            let dir = (vec_end - cell.pos).normalize();
-            let perp = Vec2 { x: -dir.y, y: dir.x };
-            let l = 0.75;
-            let c = vec_end - dir * l;
-            let arrow1 = c + perp * l * 0.5;
-            let arrow2 = c - perp * l * 0.5;
-            let arrow1 = project_to_screen(arrow1, SCALE);
-            let arrow2 = project_to_screen(arrow2, SCALE);
-
-            let girth = 1.5;
-            draw_line(screen_vec_end.x, screen_vec_end.y, arrow1.x, arrow1.y, girth, color);
-            draw_line(screen_vec_end.x, screen_vec_end.y, arrow2.x, arrow2.y, girth, color);
-
-            draw_line(screen_pos.x, screen_pos.y, screen_vec_end.x, screen_vec_end.y, girth, color);
-        }
+        grid.draw();
 
         for p in &particles {
             let part_pos = project_to_screen(p.pos, SCALE);
-            draw_circle(part_pos.x, part_pos.y, 5.0, BLUE);
+            draw_circle(part_pos.x, part_pos.y, 1.0 * SCALE, BLUE);
         }
 
         next_frame().await
